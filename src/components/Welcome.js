2c041a37d7204f9beb88a6512dfbd6e34e1b3a74
Best <irb.ossystem@gmail.com>
2020-01-25 20:28:23 +0200

    Debug fb login on mobile

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,13 +4,21 @@ import { connect } from "react-redux";
 import configs from '../configs';
 import logo from '../logo.svg';
 import { logIn } from '../actions/authAction';
+import { openAlert } from '../actions/alertAction';
+import alert from '../constants/alert';
 import '../styles/Welcome.css';
 
 const Welcome = ({ dispatch }) => {
   const responseFacebook = fbData => {
+    dispatch(openAlert(`Sucessfully loggedin`, alert.SUCCESS));
     Object.defineProperty(fbData, 'userId', Object.getOwnPropertyDescriptor(fbData, 'userID'));
     delete fbData.userID;
     dispatch(logIn(fbData));
+  }
+
+  const handleFailure = response => {
+    const msg = JSON.stringify(response) || 'EMPTY';
+    dispatch(openAlert(`Failed to login ${msg}`, alert.ERROR));
   }
 
   return (
@@ -24,6 +32,7 @@ const Welcome = ({ dispatch }) => {
           autoLoad={false}
           fields="name,email,picture"
           callback={responseFacebook}
+          onFailure={handleFailure}
           redirectUri="https://parchmentnotebook.netlify.com"
           disableMobileRedirect
         />
