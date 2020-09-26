56c9acd3a7943711c07cbf3c970ab47c00978ec9
rotimi_best <rotimiibitoyeemma@gmail.com>
2020-09-26 16:21:41 +0300

    #44: add custom service worker

---
+++ public/custom-service-worker.js
@@ -0,0 +1,10 @@
+console.log("Custom Service Worker Loaded...");
+
+self.addEventListener("push", e => {
+  const data = e.data.json();
+  console.log("Push Recieved...");
+  self.registration.showNotification(data.title, {
+    body: data.body,
+    icon: data.icon
+  });
+});
